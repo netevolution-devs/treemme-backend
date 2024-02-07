066903e7e6a1f51883920883d59a260cf20b528d
m.fochesato <m.fochesato@net-evolution.com>
2024-02-07 17:07:29 +0100

    Add JWT refresh functionality
    A new listener, JWTRefreshListener, has been created to handle JWT token expiry by generating a new one. This feature is particularly useful for users, negating the need to manually re-authenticate. Changes have also been made to the UserService to fetch users based on refresh tokens, and configuration files updated to support these functionalities.

---
+++ src/Listeners/JWTRefreshListener.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Listeners;
+use Symfony\Component\HttpFoundation\Response;
+use App\Service\UserService;
+use Symfony\Component\HttpFoundation\Cookie;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTExpiredEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+
+
+class JWTRefreshListener
+{
+    private JWTTokenManagerInterface $tokenManager;
+    private UserService $userService;
+
+
+
+
+    public function __construct(JWTTokenManagerInterface $tokenManager, UserService $userService)
+    {
+        $this->tokenManager = $tokenManager;
+        $this->userService = $userService;
+
+    }
+
+
+    /**
+     * @param JWTExpiredEvent $event
+     */
+    public function onJWTExpired(JWTExpiredEvent $event)
+    {
+            $request = $event->getRequest();
+            $refreshToken = $request->cookies->get('REFRESH_TOKEN');
+            $user = $this->getUserFromRefreshToken($refreshToken);
+
+            $newToken = $this->refreshToken($user);
+
+            $newResponse = new Response();
+            $newResponse->headers->setCookie(
+                new Cookie('BEARER', $newToken, (new \DateTime())->add(new \DateInterval('PT86400S')), '/', null, false, false)
+            );
+
+            $event->setResponse($newResponse);
+    }
+
+
+    private function refreshToken($user)
+    {
+        $newToken = $this->tokenManager->create($user);
+
+        return $newToken;
+    }
+
+    private function getUserFromRefreshToken($refreshToken)
+    {
+        $user = $this->userService->getUserByRefreshToken($refreshToken);
+
+        return $user;
+    }
+}
+
--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -2,23 +2,48 @@
 
 namespace App\Service;
 
+use App\Entity\RefreshToken;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Security;
 
 class UserService
 {
     private Security $security;
+    private EntityManagerInterface $doctrine;
 
     public function __construct(
-        Security $security
+        Security $security,
+        EntityManagerInterface $entityManager,
     )
     {
         $this->security = $security;
+        $this->doctrine = $entityManager;
     }
 
     public function getCurrentUser(): User
     {
 
         return $this->security->getUser();
+    }
+
+    public function getUserByRefreshToken($refreshToken): User | null
+    {
+        $refreshTokenObj = $this->doctrine
+            ->getRepository(RefreshToken::class)
+            ->findOneBy(['refreshToken' => $refreshToken]);
+
+        if($refreshTokenObj) {
+            $user = $this->doctrine
+                ->getRepository(User::class)
+                ->findOneBy(['email' => $refreshTokenObj->getUsername()]);
+
+
+            if($user){
+
+                return $user;}
+        }
+
+        return null;
     }
 }
