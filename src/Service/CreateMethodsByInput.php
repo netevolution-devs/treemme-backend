4b33c44cd131834856a350713948a0eda50675a7
m.fochesato <m.fochesato@net-evolution.com>
2024-05-10 16:07:51 +0200

    refactor: createMethodByInput

--- src/Service/CreateMethodsByInput.php
+++ src/Service/CreateMethodsByInput.php
@@ -2,58 +2,65 @@
 
 namespace App\Service;
 
+use DateTime;
+use Exception;
+
 class CreateMethodsByInput
 {
     /**
      * set attribute and value of given data to given entity
      *
-     * if strict == true return error string if attribute not in entity
-     * else, pass and return set entity
-     *
+     * @throws Exception
      */
-    public function createMethods( $entity, array $data): object|string
+    public function createMethods($entity, array $data): object|string
     {
-        $client_data = [];
-        foreach ($data as $field=>$value) {
-            $client_data[lcfirst(str_replace('_', '', ucwords($field, '_')))] = $value;
+        if (isset($data['lot_nr'])) {
+            $data['lot'] = $data['lot_nr'];
+            unset($data['lot_nr']);
         }
-
-
-        $attributes = get_class_methods($entity);
-
-        $keys = array_keys($client_data);
-
-        foreach ($keys as $key) {
-            if (!in_array('set' . ucfirst($key), $attributes)) {
-
-                return 'Field not found: ' .$key;
+        foreach ($data as $field => $value) {
+            $method = 'set' . str_replace('_', '', ucwords($field, '_'));
+            if (!method_exists($entity, $method)) {
+                throw new \RuntimeException($field . ' non trovato');
             }
-        }
 
-
-        foreach ($client_data as $field=>$value) {
-            if (\DateTime::createFromFormat('Y-m-d', substr($value,0,10))
-                || \DateTime::createFromFormat('d/m/Y', substr($value,0,10))
-                || \DateTime::createFromFormat('H:i', substr($value,0,10))
-                || \DateTime::createFromFormat('H:i:s', substr($value,0,10))
+            if (DateTime::createFromFormat('Y-m-d', substr($value, 0, 10))
+                || DateTime::createFromFormat('d/m/Y', substr($value, 0, 10))
+                || DateTime::createFromFormat('H:i', substr($value, 0, 10))
+                || DateTime::createFromFormat('H:i:s', substr($value, 0, 10))
             ) {
-                $value = new \DateTime($value);
+                if ($dateFromFormat = DateTime::createFromFormat('d/m/Y', substr($value, 0, 10))) {
+                    [$day, $month, $year] = explode('/', $value);
+                    if (checkdate((int)$month, (int)$day, (int)$year)) {
+                        $value = $dateFromFormat->format('Y-m-d');
+                    } else {
+                        throw new \RuntimeException('Formato data non valido');
+                    }
+                }
+                try {
+                    $reflectionMethod = new \ReflectionMethod($entity, $method);
+
+                    $params = $reflectionMethod->getParameters();
+                    $expectedType = $params[0]?->getType()?->getName();
+
+                    if ($expectedType === 'DateTime') {
+                        $value = new \DateTime($value);
+                    } else if ($expectedType === 'DateTimeImmutable') {
+                        $value = new \DateTimeImmutable($value);
+                    }
+                } catch (\Exception $e) {
+                    throw new \RuntimeException('Errore in inserimento data');
+                }
+
             } else {
-                $value = $this->detectCommaFloat($value);
+                $value_to_number = str_replace(',', '.',$value);
+                $value = is_numeric($value_to_number) ? (float)$value_to_number : $value;
             }
 
-            if (in_array('set' . ucfirst($field),get_class_methods($entity))) {
-                $entity->{'set' . ucfirst($field)}($value);
-            }
+            $entity->{$method}($value);
         }
 
         return $entity;
     }
 
-    private function detectCommaFloat($value) {
-
-        $value_to_number = str_replace(',', '.',$value);
-
-        return is_numeric($value_to_number) ? (float)$value_to_number : $value;
-    }
 }
