70c1fd6dc2e5817d9c780266a15d16191f358b3f
developer2net <developer2@net-evolution.com>
2026-01-19 10:29:40 +0100

    fix: Improve date handling and type checks in CreateMethodsByInput

--- src/Service/CreateMethodsByInput.php
+++ src/Service/CreateMethodsByInput.php
@@ -14,29 +14,28 @@ class CreateMethodsByInput
      */
     public function createMethods($entity, array $data): object|string
     {
-        if (isset($data['lot_nr'])) {
-            $data['lot'] = $data['lot_nr'];
-            unset($data['lot_nr']);
-        }
         foreach ($data as $field => $value) {
             $method = 'set' . str_replace('_', '', ucwords($field, '_'));
             if (!method_exists($entity, $method)) {
                 throw new \RuntimeException($field . ' non trovato');
             }
 
-            if (DateTime::createFromFormat('Y-m-d', substr($value, 0, 10))
-                || DateTime::createFromFormat('d/m/Y', substr($value, 0, 10))
-                || DateTime::createFromFormat('H:i', substr($value, 0, 10))
-                || DateTime::createFromFormat('H:i:s', substr($value, 0, 10))
-            ) {
+            if (is_string($value) && (
+                    DateTime::createFromFormat('Y-m-d', substr($value, 0, 10)) !== false
+                    || DateTime::createFromFormat('d/m/Y', substr($value, 0, 10)) !== false
+                    || DateTime::createFromFormat('Y-m-d H:i:s', $value) !== false
+                    || DateTime::createFromFormat('H:i', substr($value, 0, 5)) !== false
+                    || DateTime::createFromFormat('H:i:s', substr($value, 0, 8)) !== false
+                )) {
                 if ($dateFromFormat = DateTime::createFromFormat('d/m/Y', substr($value, 0, 10))) {
-                    [$day, $month, $year] = explode('/', $value);
+                    [$day, $month, $year] = explode('/', substr($value, 0, 10));
                     if (checkdate((int)$month, (int)$day, (int)$year)) {
                         $value = $dateFromFormat->format('Y-m-d');
                     } else {
                         throw new \RuntimeException('Formato data non valido');
                     }
                 }
+
                 try {
                     $reflectionMethod = new \ReflectionMethod($entity, $method);
 
@@ -47,16 +46,18 @@ class CreateMethodsByInput
                         $value = new \DateTime($value);
                     } else if ($expectedType === 'DateTimeImmutable') {
                         $value = new \DateTimeImmutable($value);
+                    } else if ($expectedType === 'DateTimeInterface') {
+                        // DateTimeInterface richiede un'implementazione concreta
+                        $value = new \DateTime($value);
                     }
                 } catch (\Exception $e) {
-                    throw new \RuntimeException('Errore in inserimento data');
+                    throw new \RuntimeException('Errore in inserimento data: ' . $e->getMessage());
                 }
 
             } else {
-                $value_to_number = str_replace(',', '.',$value);
+                $value_to_number = str_replace(',', '.', $value);
                 $value = is_numeric($value_to_number) ? (float)$value_to_number : $value;
             }
-
 
             $entity->{$method}($value);
         }
