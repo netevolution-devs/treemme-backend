a382ac4a5305bb80c65b361c5b2ff0c8b6e14831
developer2net <developer2@net-evolution.com>
2026-05-05 11:15:04 +0200

    fix(entity): correct snake_case usage in `OrderBy` annotation for `default_address` field

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -43,7 +43,7 @@ class Contact
      * @var Collection<int, ContactAddress>
      */
     #[ORM\OneToMany(mappedBy: 'contact', targetEntity: ContactAddress::class, orphanRemoval: true)]
-    #[ORM\OrderBy(['defaultAddress' => 'DESC'])]
+    #[ORM\OrderBy(['default_address' => 'DESC'])]
     #[Groups(['contact_list','contact_detail','contact_client','contact_supplier','contact_agent_list','contact_subcontractor_list', 'client_summary_print'])]
     private Collection $contactAddresses;
 
