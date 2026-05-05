f86efdaafc6971f898c823549dd9637fa5eae2fa
developer2net <developer2@net-evolution.com>
2026-05-05 11:02:55 +0200

    feat(migration, entity): add `default_address` field to ContactAddress and update related logic

---
+++ migrations/Version20260505085615.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260505085615 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE client_order_row CHANGE article_id article_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE contact_address ADD default_address TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE client_order_row CHANGE article_id article_id INT NOT NULL');
+        $this->addSql('ALTER TABLE contact_address DROP default_address');
+    }
+}
