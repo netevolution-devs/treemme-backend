f1ed3d669e895d21c4c6706bfd89569cbe8ce836
developer2net <developer2@net-evolution.com>
2026-05-18 12:08:51 +0200

    fix(entity): replace Symfony serializer with JMS serializer in ShippingCarrier

--- src/Entity/ShippingCarrier.php
+++ src/Entity/ShippingCarrier.php
@@ -6,8 +6,8 @@ use App\Repository\ShippingCarrierRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Groups;
 
-use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ShippingCarrierRepository::class)]
 class ShippingCarrier
